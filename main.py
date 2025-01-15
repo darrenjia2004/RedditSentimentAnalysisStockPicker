96b75275fb13fa77a386c37b3e4e11a9c9da4d19
darrenjia2004 <darrenjia2004@gmail.com>
2025-01-15 05:12:24 -0500

    RAHHHH

--- main.py
+++ main.py
@@ -1,35 +1,78 @@
+import praw
+import config
+from nltk.sentiment import SentimentIntensityAnalyzer
+import re
+import pickle
 from alpaca.trading.client import TradingClient
 from alpaca.trading.enums import OrderSide, TimeInForce
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.stream import TradingStream
 import config
-import asyncio
+
+reddit = praw.Reddit(
+    client_id=config.REDDIT_CLIENT_ID,
+    client_secret=config.REDDIT_CLIENT_SECRET,
+    user_agent=config.REDDIT_USER_AGENT,
+    username=config.REDDIT_USERNAME,
+    password=config.REDDIT_PASSWORD
+)
 
 client = TradingClient(config.ALPACA_API_KEY, config.ALPACA_SECRET_KEY, paper=True)
-acc = dict(client.get_account())
-# for k,v in acc.items():
-#     print(f"{k:30}{v}")
 
-order_details = MarketOrderRequest(
-    symbol = "SPY",
-    qty = 1,
-    side = OrderSide.BUY,
-    time_in_force = TimeInForce.DAY
-)
-# order = client.submit_order(order_data = order_details)
+def getStockSentiments(posts, tickers):
+    sid = SentimentIntensityAnalyzer()
 
+    ticker_dict = {ticker: [] for ticker in tickers}
 
-# async def trade_status(data):
-#     print(data)
+    tickers_delimited = "|".join(ticker for ticker in tickers)
+    pattern = fr'\b({tickers_delimited})\b'
 
-# trades = TradingStream(config.API_KEY, config.SECRET_KEY, paper=True)
-# trades.subscribe_trade_updates(trade_status)
-# trades.run()
+    print('Finding posts with stock tickers:')
 
-assets = [asset for asset in client.get_all_positions()]
-positions = [(asset.symbol, asset.qty, asset.current_price) for asset in assets]
-print("Positions")
-print(f"{'Symbol':9}{'Quantity':>4}{'Value':>15}")
-print("-" * 28)
-for position in positions:
-    print(f"{position[0]:9}{position[1]:>4}{float (position[1]) * float(position[2]):>15.2f}")
+    for post in posts:
+        text = post.title
+        matches = set(re.findall(pattern, text))
+
+        if (matches):
+            sentiment_score = sid.polarity_scores(text)['compound']
+            print(f'{text:<150}{",".join(match for match in matches):>20}')
+            for match in matches:
+                ticker_dict[match].append(sentiment_score)
+
+    return ticker_dict
+
+def getHighestAvgSentiment(sentiments):
+    highest_avg_key = None
+    highest_avg_value = float('-inf')
+
+    for key, value in sentiments.items():
+        if (value):
+            avg_sentiment = sum(value) / len(value)
+            if avg_sentiment > highest_avg_value:
+                highest_avg_value = avg_sentiment
+                highest_avg_key = key
+
+    return highest_avg_key
+
+if __name__ == "__main__":
+    with open('tickers.pkl', 'rb') as f:
+        tickers = pickle.load(f)
+
+    subreddit = reddit.subreddit('wallstreetbets')
+    top_posts = subreddit.top(time_filter = 'week', limit=None)
+
+    sentiments = getStockSentiments(top_posts, tickers)
+
+    stockToBuy = getHighestAvgSentiment(sentiments)
+
+    print(f'Submitting Buy Order for: {stockToBuy}')
+
+    order_details = MarketOrderRequest(
+        symbol = stockToBuy,
+        notional = 1000,
+        side = OrderSide.BUY,
+        time_in_force = TimeInForce.DAY
+    )
+
+    order = client.submit_order(order_data = order_details)
+    
---
+++ tickers.py
@@ -0,0 +1,19 @@
+import csv
+import pickle
+
+# Read tickers from CSV
+tickers = []
+with open('tickers.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        ticker = row[0].split(',')[0].strip('"')
+        tickers.append(ticker)
+
+# Remove common words from the list
+excludedTickers = ['AI']
+for ticker in excludedTickers:
+    tickers.remove(ticker)
+
+# Save tickers to a pickle file
+with open('tickers.pkl', 'wb') as f:
+    pickle.dump(tickers, f)
